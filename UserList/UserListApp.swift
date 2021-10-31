7c0dda0f9e5ea41008dc705d53d330b06de74fc9
Connor Boothe <connorboothe@gmail.com>
2021-10-31 14:57:37 -0400

    Initial Commit

---
+++ UserList/UserListApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserListApp.swift
+//  UserList
+//
+//  Created by Connor Boothe on 10/31/21.
+//
+
+import SwiftUI
+
+@main
+struct UserListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
